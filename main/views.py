6595e958a60e6bca7aefc1e9761e474f1e0b18ff
pavel <pashasil-5@mail.ru>
2025-11-20 14:45:38 +0300

    Update ALLOWED_HOSTS

---
+++ main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+def index(request): return render(request, "index.html")
+def about(request): return render(request, "about.html")
+def services(request): return render(request, "services.html")
+def portfolio(request): return render(request, "portfolio.html")
+def contacts(request): return render(request, "contacts.html")
